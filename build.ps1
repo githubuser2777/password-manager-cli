c59a49d80833d96e5316fd818b726af7176913a7
githubuser2777 <vuvudaiop@gmail.com>
2026-06-29 22:08:15 +0700

    chore: remove redundant config and build scripts

--- build.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Building Password Manager CLI..."
-go build -o passmgr.exe
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "Build successful: passmgr.exe" -ForegroundColor Green
-} else {
-    Write-Host "Build failed!" -ForegroundColor Red
-}
